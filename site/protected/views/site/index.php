d30428102e51cfa82c7bfabfef5b5a711c6cce04
crazyyy <crazyyy@gmail.com>
2014-08-28 08:54:27 +0300

    kraken images, end editing main page

--- site/protected/views/site/index.php
+++ site/protected/views/site/index.php
@@ -1,81 +1,81 @@
 <?php
 /* @var $this SiteController */
-
-$this->pageTitle=Yii::app()->name;
-$file=__FILE__;
-$layout=$this->getLayoutFile('main');
+    $this->pageTitle=Yii::app()->name;
+    $file=__FILE__;
+    $layout=$this->getLayoutFile('main');
 ?>
 
 <div class="form_wrapper cf5af41">
     <form id="" action="/search" method="get">
         <ul>
-            <li class="padT10">
-                <img src="/images/zoom.png" alt="" class="marR10">Найти город или отель:<br>
-                <input type="text" id="" name="s" class="inp" placeholder="" autocomplete="off" spellcheck="false" required="required">
+            <li>
+                <label for="scity">Найти город или отель:</span>
+                <input typte="text" id="scity" name="s" placeholder="" autocomplete="off" spellcheck="false" required="required">
             </li>
-            <!--для скрытия ошибки добавить в <li> класс dispNone-->
-            <li class="padT10  dispNone">
-                <img src="images/info.png" alt="" class="marR10 v-aM floatL"><div class="info">В этом отеле нет ни одного свободного номера на выбранные даты</div>
-            </li>
-            <li class="padT10 ">Прибытие<br>
-                <div class="new-select-style-wpandyou w55 marR10">
-                <select class="w80" name="day_arr">
-                    <?for ($i=1;$i<=31;$i++){?>
-                        <option <? if($i==date('d')): ?> selected <? endif ?> value="<?if ($i<10) echo '0';echo $i?>"><?=$i?></option>
-                    <?}?>
-                </select>
-                </div>
-                <div class="new-select-style-wpandyou w170">
-                <select class="w195" name="month_year_arr">
-                    <?for ($i=$monthNumber;$i<$monthNumber+12;$i++){?>
-                                                                <?$yearCurrent=$year?>
-                                                                <?$index=$i%12?>
-                                                                <?if($index==0) $index=12?>
-                                                                <option value="<?=$yearCurrent?>-<?if ($index<10) echo '0'?><?=$index?>"><?=Yii::app()->controller->monthes[$index]?> <?=$yearCurrent?></option>
-                                                                <?if($index==12) $yearCurrent++?>
-                                                            <?}?>
-                </select>
-                    </div>
-            </li>
-            <li class="padT10 ">Отъезд<br>
-                <div class="new-select-style-wpandyou w55 marR10">
-                <select class="w80" name="day_dep">
+            <li class="empty_hotel dispNone">
+                <span>В этом отеле нет ни одного свободного номера на выбранные даты</span>
+            </li><!-- empty_hotel -->
+            <li class="search_arrive">
+                <label for="day_arr">Прибытие</label>
+                    <select id="day_arr" name="day_arr">
+                        <?for ($i=1;$i<=31;$i++){?>
+                            <option <? if($i==date('d')): ?> selected <? endif ?> value="<?if ($i<10) echo '0';echo $i?>"><?=$i?></option>
+                        <?}?>
+                    </select>
+                    <select name="month_year_arr">
+                        <?for ($i=$monthNumber;$i<$monthNumber+12;$i++){?>
+                        <?$yearCurrent=$year?>
+                        <?$index=$i%12?>
+                        <?if($index==0) $index=12?>
+                        <option value="<?=$yearCurrent?>-<?if ($index<10) echo '0'?><?=$index?>"><?=Yii::app()->controller->monthes[$index]?> <?=$yearCurrent?></option>
+                        <?if($index==12) $yearCurrent++?>
+                        <?}?>
+                    </select>
+            </li><!-- search_arrive -->
+            <li class="search_dep">
+                <label for="day_dep">Отъезд</label>
+                <select id="day_dep" name="day_dep">
                     <?for ($i=31;$i>0;$i--){?>
                     <option value="<?if ($i<10) echo '0';echo $i?>"><?=$i?></option>
                     <?}?>
                 </select>
-                </div>
-                <div class="new-select-style-wpandyou w170">
-                <select class="w195" name="month_year_dep">
+                <select name="month_year_dep">
                     <?for ($i=$monthNumber;$i<$monthNumber+12;$i++){?>
-                        <?$index=$i%12?>
-                        <?if($index==0) $index=12?>
-                        <option value="<?=$year?>-<?if ($index<10) echo '0'?><?=$index?>"><?=Yii::app()->controller->monthes[$index]?> <?=$year?></option>
-                        <?if($index==12) $year++?>
+                    <?$index=$i%12?>
+                    <?if($index==0) $index=12?>
+                    <option value="<?=$year?>-<?if ($index<10) echo '0'?><?=$index?>"><?=Yii::app()->controller->monthes[$index]?> <?=$year?></option>
+                    <?if($index==12) $year++?>
                     <?}?>
                 </select>
-                </div>
-            </li>
-            <li class="padT10">
-                <div class="center padTB40">
-                    <input id="" type="submit" value="Найти" class="button_green">
-                </div>
+            </li><!-- search_dep -->
+            <li>
+                <button id="" type="submit" class="button_green">Найти</button>
             </li>
         </ul>
     </form>
 </div>
-<div class="main_wrapper">
+<ul class="search_citywnumbers">
     <?foreach($data[0]['data'] as $city):?>
-        <a href="/search?s=<?=$city['name']?>"><div class="city"><?=$city['name']?><span><?=$city['nr_hotels']?></span><img src="/images/str.png" alt="" class="floatR marT5R10"></div></a>
+    <li>
+        <a href="/search?s=<?=$city['name']?>">
+            <?=$city['name']?><span><?=$city['nr_hotels']?></span>
+        </a>
+    </li>
     <?endforeach;?>
-    <?foreach($data[1]['data'] as $region):?>
-        <a href="/search?s=<?=$region['name']?>"><div class="city"><?=$region['name']?><span><?=$region['count']?></span><img src="/images/str.png" alt="" class="floatR marT5R10"></div></a>
+    <?foreach($data[1]['data'] as $region):?>  
+    <li>
+        <a href="/search?s=<?=$region['name']?>">
+            <?=$city['name']?><span><?=$city['nr_hotels']?></span>
+        </a>
+    </li>
     <?endforeach;?>
-<h1>Почему нас выбирают?</h1>
+</ul><!-- search_citywnumbers -->
+<div class="home_aftersearch">
+    <h1>Почему нас выбирают?</h1>
     <ul>
-        <img src="/images/11.png" alt="" class="floatL v-aM"><li>Абсолютно бесплатные услуги бронирования</li>
-        <img src="/images/14.png" alt="" class="floatL"><li>Отзывы о гостиницах: более 8000 людей написали свои впечатления для вас </li>
-        <img src="/images/13.png" alt="" class="floatL"><li>Удобный выбор из 2160 гостиниц в 164 городах Украины</li>
-        <img src="/images/12.png" alt="" class="floatL"><li>Круглосуточная поддержка по телефону, без выходных.</li>
+        <li>Абсолютно бесплатные услуги бронирования</li>
+        <li>Отзывы о гостиницах: более 8000 людей написали свои впечатления для вас </li>
+        <li>Удобный выбор из 2160 гостиниц в 164 городах Украины</li>
+        <li>Круглосуточная поддержка по телефону, без выходных.</li>
     </ul>
-</div>
+</div><!-- main_wrapper -->
